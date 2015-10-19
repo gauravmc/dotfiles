0d3c180aa8d10e570c2aeed7237b54a0ed362fb3
Gaurav Chande <gmail@gauravchande.com>
2015-10-19 15:18:11 +0000

    No .irbrb

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -3,7 +3,7 @@
 dir=~/dotfiles
 olddir=~/dotfiles_old
 folders=".bundle"
-files=".bashrc .vimrc .bash_profile .zshrc .ctags .gitconfig .bundle/config .agignore .gitignore_global .tmux.conf .irbrc .pryrc .gemrc"
+files=".bashrc .vimrc .bash_profile .zshrc .ctags .gitconfig .bundle/config .agignore .gitignore_global .tmux.conf .pryrc .gemrc"
 
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
