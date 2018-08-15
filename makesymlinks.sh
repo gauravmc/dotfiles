c675d6384fa64acbc4c5efb282324c0d977ce6ec
Gaurav Chande <gmail@gauravchande.com>
2018-08-15 17:52:45 +0530

    Update script after removing files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -3,7 +3,7 @@
 dir=~/dotfiles
 olddir=~/dotfiles_old
 folders=".bundle"
-files=".bashrc .vimrc .bash_profile .zshrc .ctags .gitconfig .bundle/config .agignore .gitignore_global .tmux.conf .pryrc .gemrc"
+files=".vimrc .zshrc .ctags .gitconfig .bundle/config .agignore .gitignore_global .pryrc .gemrc"
 
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
